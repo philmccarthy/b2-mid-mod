99503cdfeaa63b7042a314f5514a2ea694879311
Phil McCarthy <philmcc2631@gmail.com>
2021-01-04 11:57:04 -0700

    Add code for uniq_actor_names movie class method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def self.uniq_actor_names(actor_name)
+    joins(:actors)
+      .where.not('actors.name = ?', actor_name)
+      .select('actors.name')
+      .distinct
+      .pluck('actors.name')
+  end
 end
