23f1eddfbbb6dd5c5c13e0f21d6ea57dcfb9f64c
Phil McCarthy <philmcc2631@gmail.com>
2021-01-04 09:35:51 -0700

    Add database setup with model relationship tests passing

---
+++ spec/models/movie_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+  describe 'relationships' do
+    it { should belong_to :studio }
+    it { should have_many :movie_actors }
+    it { should have_many(:actors).through(:movie_actors) }
+  end
+
+  # describe 'instance methods' do
+  # end
+end
