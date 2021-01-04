e8df94bcb61b28b1d5d0ee751605fef329c5898c
Phil McCarthy <philmcc2631@gmail.com>
2021-01-04 10:26:37 -0700

    Add model spec for actor scopes avg_age and order_by_age

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -5,4 +5,20 @@ RSpec.describe Actor, type: :model do
     it { should have_many :movie_actors }
     it { should have_many(:movies).through(:movie_actors) }
   end
+
+  describe 'scopes' do
+    before(:each) do
+      @fox = Actor.create!(name: 'Jamie Fox', age: 53)
+      @fey = Actor.create!(name: 'Tina Fey', age: 50)
+    end
+
+    it 'avg_age' do
+      expect(Actor.avg_age).to eq((@fox.age.to_f + @fey.age) / 2)
+    end
+
+    it 'order_by_age' do
+      expect(Actor.order_by_age.first).to eq(@fey)
+      expect(Actor.order_by_age.last).to eq(@fox)
+    end
+  end
 end
