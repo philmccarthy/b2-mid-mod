2bdc5bdf4b8e574c1b2e3eef1b35a18ecd90653b
Phil McCarthy <philmcc2631@gmail.com>
2021-01-04 11:20:59 -0700

    Add code for class method, previous commit was spec only

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,4 +4,8 @@ class Actor < ApplicationRecord
 
   scope :order_by_age, -> { order(age: :asc) }
   scope :avg_age, -> { average(:age) }
+
+  def self.by_name(actor_name)
+    where(name: actor_name).first
+  end
 end
