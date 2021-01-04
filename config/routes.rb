e98f22101e82a650f5257138c63d5ca51ce1fcd3
Phil McCarthy <philmcc2631@gmail.com>
2021-01-04 11:56:30 -0700

    Add actors show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :studios, only: [:index]
   resources :movies, only: [:show]
   resources :movie_actors, only: [:create]
+  resources :actors, only: [:show]
 end
