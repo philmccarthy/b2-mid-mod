0d29b3007b1a77afa6f3d3491d3912db8bd791ca
Phil McCarthy <philmcc2631@gmail.com>
2021-01-04 10:26:54 -0700

    Add actor model scopes avg_age and order_by_age

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,7 @@
 class Actor < ApplicationRecord
   has_many :movie_actors
   has_many :movies, through: :movie_actors
+
+  scope :order_by_age, -> { order(age: :asc) }
+  scope :avg_age, -> { average(:age) }
 end
