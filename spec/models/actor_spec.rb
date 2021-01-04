92f32d9e8372d9e82f51febd6e66946d08f662a0
Phil McCarthy <philmcc2631@gmail.com>
2021-01-04 11:20:35 -0700

    Add actor class method to find record by_name

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -6,19 +6,24 @@ RSpec.describe Actor, type: :model do
     it { should have_many(:movies).through(:movie_actors) }
   end
 
-  describe 'scopes' do
+  describe 'class methods' do
     before(:each) do
+      Actor.destroy_all
       @fox = Actor.create!(name: 'Jamie Fox', age: 53)
       @fey = Actor.create!(name: 'Tina Fey', age: 50)
     end
 
-    it 'avg_age' do
+    it 'scope: avg_age' do
       expect(Actor.avg_age).to eq((@fox.age.to_f + @fey.age) / 2)
     end
 
-    it 'order_by_age' do
+    it 'scope: order_by_age' do
       expect(Actor.order_by_age.first).to eq(@fey)
       expect(Actor.order_by_age.last).to eq(@fox)
+    end
+
+    it '::by_name' do
+      expect(Actor.by_name('Jamie Fox')).to eq(@fox)
     end
   end
 end
